a2d09e7e9c4193caf913173883f589d559ef7bcc
Cameron Jensen and Riley Watts <cameron@>
2017-03-22 10:41:20 -0700

    reformat templates, add new routes to app.java

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -51,4 +51,11 @@ public class TaskTest {
     Task myTask = new Task("Mow the lawn");
     assertEquals(1, myTask.getId());
   }
+
+  @Test
+  public void find_returnsTaskWithSameId_secondTask() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Buy groceries");
+    assertEquals(Task.find(secondTask.getId()), secondTask);
+  }
 }
